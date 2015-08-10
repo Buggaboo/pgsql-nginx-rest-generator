28e1878e3517bc5d1a68a4e845597fefc2e849fc
Jasm Sison <j.sison@itude.com>
2015-08-10 13:15:15 +0200

    Apparently when using string#format, { must be escaped with {{... doh

--- pgsql-nginx-rest-gen.py
+++ pgsql-nginx-rest-gen.py
@@ -53,21 +53,21 @@ template = """ \
 # WARNING: DO NOT USE IN PRODUCTON AS-IS. For security's sake, at least use BasicAuth.
 worker_processes 8;
 
-events {}
+events {{}}
 
-http {
-  upstream database {
+http {{
+  upstream database {{
     # TODO also determine from the command-line: localhost? dbname? auth?
     postgres_server 127.0.0.1 dbname=articledb user=username password=yourpass;
-  }
+  }}
   
-  server {
+  server {{
     # TODO see previous comment
     # TODO steal the best ideas from OData
     listen       8080;
     server_name  localhost;
 
-    location /{table_name} {
+    location /{table_name} {{
       postgres_pass database;
       rds_json on;
       postgres_query    HEAD GET  "SELECT * FROM {table_name}";
@@ -78,9 +78,9 @@ http {
       postgres_query
         POST "INSERT INTO {table_name} {select_args} VALUES{prefixed_select_args} RETURNING *";
       postgres_rewrite  POST changes 201;
-    }
+    }}
 
-    location ~ /{table_name}/(?<id>\d+) {
+    location ~ /{table_name}/(?<id>\d+) {{
       postgres_pass database;
       rds_json  on;
       # TODO determine if id is the actual default id name # do the primary key check info I dumped...
@@ -97,30 +97,32 @@ http {
       postgres_query    DELETE  "DELETE FROM {table_name} WHERE id=$escaped_id";
       postgres_rewrite  DELETE  no_changes 410;
       postgres_rewrite  DELETE  changes 204;
-    }
-  }
-}
+    }}
+  }}
+}}
+
+# TODO generate wget and curl commands
 """
+
+
 
 import sys
 import sqlparse
 
-
 def format_template(_table_name, column_names):
-	_escape_select_args = '\n'.join([ 'postgres_escape ${name} $arg_{name};'.format(name=c) for c in column_names ])
+	_escape_select_args = '\n'.join([ '      postgres_escape ${name} $arg_{name};'.format(name=c) for c in column_names ])
 	_select_args = '(%s)' % ', '.join(column_names)
         _prefixed_select_args = '(%s)' % ', '.join([ '$' + c for c in column_names ])
         _update_args = ', '.join([ "{name}=${name}".format(name=c) for c in column_names ])
-	instance = template.format(table_name=_table_name, escape_select_args=_select_args, select_args=_select_args, prefixed_select_args=_prefixed_select_args, update_args=_update_args)
-	print instance
+	print template.format(table_name=_table_name, escape_select_args=_escape_select_args, select_args=_select_args, prefixed_select_args=_prefixed_select_args, update_args=_update_args)
 	
 
 def generate_nginx_conf(create_statement):
 	group_token = str([ t for t in create_statement.tokens if not t.is_whitespace() and t.is_group() ][0]).split('\n')
 	table_name = group_token[0]
+	table_name = table_name.replace('(', '')
 	# skip '('
-	column_dict = {}
-	parsed_columns = [ column_def.strip().split(' ')[0] for column_def in group_token[2:] if not column_def == ')']
+	parsed_columns = [ column_def.strip().split(' ')[0] for column_def in group_token[1:] if not column_def == ')' and not column_def == '(' ]
 	column_names = [ c for c in parsed_columns if not c == 'CONSTRAINT' ] # TODO
 	format_template(table_name, column_names)
 			
